217f2c015e750d02f1ac961fced02a1326bb5b2c
Kevin <kienluproductions@gmail.com>
2026-04-12 16:08:09 +0700

    second commit

--- app/src/main/java/com/example/notitracker/service/MyNotificationListener.kt
+++ app/src/main/java/com/example/notitracker/service/MyNotificationListener.kt
@@ -1,4 +1,4 @@
-package com.example.notihub.service
+package com.example.notitracker.service
 
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
@@ -16,9 +16,15 @@ class MyNotificationListener : NotificationListenerService() {
         Log.d("NOTI", "App: $packageName")
         Log.d("NOTI", "Title: $title")
         Log.d("NOTI", "Text: $text")
+        Log.d("NOTI_DEBUG", "App: $packageName | $title - $text")
+
     }
 
     override fun onNotificationRemoved(sbn: StatusBarNotification) {
         Log.d("NOTI", "Notification removed")
+    }
+
+    override fun onListenerConnected() {
+        Log.d("NOTI", "CONNECTED ✅")
     }
 }
